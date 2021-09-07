04ec0f3db73b004e3dd93d9aaefd8eb2106259bd
Rhaynner Liberato <rhay_liberato@hotmail.com>
2021-09-07 00:32:40 -0300

    App static na nuvem!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <h1 className="text-primary py-3">Dashboard de vendas</h1>
 
         <div className="row px-3">
           <div className="col-sm-6">
