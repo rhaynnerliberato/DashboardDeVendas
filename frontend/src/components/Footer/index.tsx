c692612160749436cafe2a3a4fc4b4b6aeec2b4f
Rhaynner Liberato <rhay_liberato@hotmail.com>
2021-09-06 23:40:20 -0300

    basic statics components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/rhaynnerliberato" target="_blank" rel="noreferrer">Rhaynner Liberato</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
